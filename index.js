b45227a30d6c95bfb3ea4a108362b2b40222b435
Victor Aguiar <victoraguiarrosa397@gmail.com>
2022-10-14 18:57:27 -0300

    Js yey

---
+++ index.js
@@ -0,0 +1,52 @@
+function generate(){
+	let usersConfig = {
+		colors: ['#64AFE5', '#F07E31'],
+		pfps: ['wabboffet.png', 'blaze.jpg'],
+		names: ['Dundundont', 'Carriola'],
+		status: ['Jogando <strong>Pokemon Ruby</strong>', null],
+		tags: ['1985', '2005'],
+		desc: ["Hello! I'm Dundundont.<br>He/Him<br>PT-BR", "Olá! Eu sou Carlos, mas pode me chamar de Carriola.<br>He/Him | They/Them<br>PT-PT"],
+		aquan: ["Dec 30, 2017", "Oct 20, 2018"]
+	}
+	
+	for(c=0;c<usersConfig.names.length;c++){
+		let user = document.createElement('div');
+		user.setAttribute('id', 'userContainer');
+		user.classList.add('userContainer');
+		user.innerHTML = `
+		<div id="userContainer" class="userContainer">
+				<div id="userMainHeader${c}" class="userMainHeader">
+					<img src="${usersConfig.pfps[c]}" alt="Profile Picture">
+					<div id="icon" class="icon"></div>
+				</div>
+				<div id="userBodyContainer" class="userBodyContainer">
+					<div id="userMainBody" class="userMainBody">
+						<div id="userHeader" class="userHeader">
+							<div id="nickContainer" class="nickContainer">
+								<h1 class="nick">${usersConfig.names[c]}</h1><h1 id="id" class="id">#${usersConfig.tags[c]}</h1>
+							</div>
+							<p id="status${c}" class="status">${usersConfig.status[c]}</strong></p>
+						</div>
+						<div id="userDes" class="userDes">
+							<h2 class="categories">ABOUT ME</h1>
+							<p class="desc">${usersConfig.desc[c]}</p>
+							<h2 class="categories">DISCORD MEMBER SINCE</h2>
+							<p class="dms">${usersConfig.aquan[c]}</p>
+						</div>
+						<div id="userBody">
+							<h2 class="categories">NOTE</h2>
+							<input type="text" id="note" class="note" placeholder="Click to add a note">
+							<input type="text" id="message" class="message" placeholder="Message @${usersConfig.names[c]}">
+						</div>
+					</div>
+				</div>
+			</div>
+		`;
+		
+		document.body.appendChild(user);
+		document.getElementById(`userMainHeader${c}`).style.backgroundColor = `${usersConfig.colors[c]}`;
+		if(usersConfig.status[c] == null){
+			document.getElementById(`status${c}`).remove();
+		}
+	}
+}
